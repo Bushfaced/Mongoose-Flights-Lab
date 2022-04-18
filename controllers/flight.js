e1d686f328b0e395ba4a090b732da39d944452dc
richardguzman <thisisrichardguzman@gmail.com>
2022-04-18 15:20:07 -0700

    setup still

---
+++ controllers/flight.js
@@ -0,0 +1,42 @@
+var Flight = require('../models/flights');
+
+module.exports = {
+  index,
+  show,
+  new: newFlight,
+  create
+};
+
+function index(req, res) {
+  Flight.find({}, function(err, flights) {
+    res.render('flights/index', { title: 'All Flights', flights });
+  });
+}
+
+function show(req, res) {
+  Flight.findById(req.params.id, function(err, flight) {
+    res.render('flights/show', { title: 'Flight Detail', flight });
+  });
+}
+
+function newFlight(req, res) {
+  res.render('flights/new', { title: 'Add Flight' });
+}
+
+function create(req, res) {
+  req.body.nowShowing = !!req.body.nowShowing;
+  req.body.cast = req.body.cast.replace(/\s*,\s*/g, ',');
+  // split if it's not an empty string
+  if (req.body.cast) req.body.cast = req.body.cast.split(',');
+  for (let key in req.body) {
+    if (req.body[key] === '') delete req.body[key];
+  }
+  var flight = new Flight(req.body);
+  flight.save(function(err) {
+    // one way to handle errors
+    if (err) return res.redirect('/flights/new');
+    console.log(movie);
+    // for now, redirect right back to new.ejs
+    res.redirect('/flights');
+  });
+}
--- routes/flights.js
+++ routes/flights.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const flightsCtrl = require('../controllers/flight');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// All routes starts with: /movies (because of the mounting in server.js)
+
+// GET /movies
+router.get('/', flightsCtrl.index);
+// GET /movies/new (new functionality)
+router.get('/new', flightsCtrl.new);
+// GET /movies/:id (show functionality)
+router.get('/:id', flightsCtrl.show);
+// POST /movies (create functionality)
+router.post('/', flightsCtrl.create);
 
 module.exports = router;
