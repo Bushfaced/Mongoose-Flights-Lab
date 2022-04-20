6dea60e8e6ebaf8aa9157f7220cd9d857e1522db
richardguzman <thisisrichardguzman@gmail.com>
2022-04-20 11:05:12 -0700

    nope

---
+++ crud.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+require('./config/database');
+
+const Movie = require('./models/movie');
+const Performer = require('./models/performer');
+
+let movies;
+
+Movie.find({}, function(err, movieDocs) {
+  movies = movieDocs;
+});
+
