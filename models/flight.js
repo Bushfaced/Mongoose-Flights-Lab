729e7f4781e21b4fd80fc6775fbe9e54d0e3fc64
richardguzman <thisisrichardguzman@gmail.com>
2022-04-21 14:21:40 -0700

    halfway home

--- models/flight.js
+++ models/flight.js
@@ -1,53 +1,39 @@
 const mongoose = require('mongoose');
-// Optional shortcut to the mongoose.Schema class
+// optional shortcut to the mongoose.Schema class
 const Schema = mongoose.Schema;
 
-// const reviewSchema = new Schema({
-//   rating: {
-//     type: Number,
-//     min: 1,
-//     max: 5,
-//     default: 5
-//   }
-// }, {
-//   // createdAt & updatedAt properties
-//   timestamps: true
-// });
+const destinationSchema = {
+    airport: {
+        type: String,
+        enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN'],
+    },
+    arrival: {
+        type: Date,
+    }
+}
 
 const flightSchema = new Schema({
-  airline: {
-    type: String,
-    enum: ['American', 'Southwest', 'United']
-  },
-  airport: {
-    type: String,
-    enum: ['Aus', 'DFW', 'DEN', 'LAX', 'SAN'],
-    default: 'DEN'
-  },
-  content: {
-    type: Number,
-    match: /(10|[1-9][0-9]|[1-9][0-9][0-9]|[1-9][0-9][0-9][0-9])/
-  },
-  // title: {
-  //   type: String,
-  //   required: true
-  // },
-  // releaseYear: {
-  //   type: Number,
-  //   default: function() {
-  //     return new Date().getFullYear();
-  //   }
-  // },
-  // cast: [{type: Schema.Types.ObjectId, ref: 'Performer'}],
-  // nowShowing: {
-  //   type: Boolean,
-  //   default: false
-  // },
-  // reviews: [reviewSchema]
-}, {
-  // Mongoose will automatically add and maintain
-  // a createdAt and updatedAt property on the docs
-  timestamps: true
+    airline: {
+        type: String,
+        enum: ['American', 'Southwest', 'United']
+    },
+    airport: {
+        type: String,
+        enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN'],
+        default: 'DEN'
+    },
+    flightNo: {
+        type: Number,
+        required: true,
+        min: 10,
+        max: 999
+    },
+    departs: {
+        type: Date,
+        default: Date.now() + 365*24*60*60000,
+    },
+    destinations: [destinationSchema],
 });
+
 
 module.exports = mongoose.model('Flight', flightSchema);
